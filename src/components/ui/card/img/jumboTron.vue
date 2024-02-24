14535b4c03ee1daccaed40a2724d4def10d7b9f7
nekagit <nenad99@hotmail.de>
2024-02-24 22:44:16 +0100

    init

---
+++ src/components/ui/card/img/jumboTron.vue
@@ -0,0 +1,24 @@
+<script>
+</script>
+<template>
+<section class="jumboTron">
+    <!-- <img src="https://learning-zone.github.io/website-templates/above-educational-bootstrap-responsive-template/img/slides/1.jpg" alt="asdf"/> -->
+</section>
+
+
+</template>
+<style scoped >
+.jumboTron {
+      width: 100%;
+      height: 100%;
+      background: url("../../../../assets/SVG/border-edge-nine-divided.png");
+      background-repeat:no-repeat;
+  background-size: contain;
+}
+
+img {
+ 
+  background-repeat: no-repeat;
+  background-size: contain;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,30 @@
 <script setup lang="ts">
-import AppleNav from "@/components/ui/nav/AppleNav.vue";
+import JumboTron from '@/components/ui/card/img/jumboTron.vue';
 </script>
 <template>
-<main style="position: absolute; top: 0; left: 0; width: 80%;height:80vh;background-color: antiquewhite;">
-<section style="width: 80%; height:50vh;margin: 4rem; border:1px solid black; background-color: white;">
-  
-  <AppleNav />
-
-</section>
-</main>
+  <main class="site">
+    <section class="mainSection">
+      <JumboTron />
+    </section>
+  </main>
 </template>
 <style scoped>
+.site {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 95%;
+  height: 95vh;
+  background-color: antiquewhite;
+}
+
+.mainSection {
+  width: 90%;
+  height: 80vh;
+  margin: 1rem;
+  padding: 14px;
+  border: 1px solid black;
+  background-color: white;
+}
+
 </style>
